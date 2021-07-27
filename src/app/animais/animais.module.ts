acd221f483566b59928ca66dc1394880682a8310
Matheus B. Grigoletto <matheus.grigoletto@gmail.com>
2021-07-27 08:09:31 -0300

    Curso "Angular: Controle o fluxo de navegação" - Aula 2 - Lista de animais
    Nesta aula, aprendemos:
    
    - Como criar uma página utilizando diversos componentes reutilizáveis utilizando a composição.
    - Aprendemos a utilizar o operador switchMap para concatenar dois fluxos de informações e não realizar um subscribe dentro de outro subscribe.
    - Aprendemos a utilizar guarda de rotas para controlar o fluxo de navegação do usuário em nossa aplicação.

--- src/app/animais/animais.module.ts
+++ src/app/animais/animais.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CartaoModule } from '../componentes/cartao/cartao.module';
 import { AnimaisRoutingModule } from './animais-routing.module';
 import { ListaComponent } from './lista/lista.component';
 import { AnimalComponent } from './animal/animal.component';
+import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
 
 @NgModule({
-  declarations: [ListaComponent, AnimalComponent],
-  imports: [CommonModule, AnimaisRoutingModule],
+  declarations: [ListaComponent, AnimalComponent, GradeFotosAnimaisComponent],
+  imports: [CommonModule, AnimaisRoutingModule, CartaoModule],
 })
 export class AnimaisModule {}
--- src/app/animais/lista/lista.component.ts
+++ src/app/animais/lista/lista.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { UsuarioService } from 'src/app/auth/usuario/usuario.service';
+
+import { Animais } from '../animais';
+import { AnimaisService } from '../animais.service';
 
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
-  styleUrls: ['./lista.component.scss']
+  styleUrls: ['./lista.component.scss'],
 })
 export class ListaComponent implements OnInit {
+  animais$!: Observable<Animais>;
 
-  constructor() { }
+  constructor(
+    private usuarioService: UsuarioService,
+    private animaisService: AnimaisService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.animais$ = this.usuarioService.retornaUsuario().pipe(
+      switchMap((usuario) => {
+        const userName = usuario.name ?? '';
+        return this.animaisService.listaUsuario(userName);
+      })
+    );
   }
-
 }
